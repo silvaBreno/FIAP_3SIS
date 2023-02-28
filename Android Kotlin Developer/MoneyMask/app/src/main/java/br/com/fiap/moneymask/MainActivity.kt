4fa01d7913067ab9ab3b0c3b1347f92b3c45b30d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-02-27 21:00:46 -0300

    Aula Kotlin Money Mask

---
+++ Android Kotlin Developer/MoneyMask/app/src/main/java/br/com/fiap/moneymask/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.com.fiap.moneymask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import br.com.fiap.moneymask.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+//    val toggleButton : Button = findViewById(R.id.toggle_button)
+    lateinit var binding: ActivityMainBinding
+    private val mask: String = "******"
+    private val value: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //        toggle_button.setText("Text")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.moneyCurrency
+        setContentView(binding.root)
+    }
+
+    fun clickToggleButton(){
+        value?.length
+        binding.toggleButton.setOnClickListener{
+
+        }
+    }
+}
