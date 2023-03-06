b1b23fe9f79236f7ef5a24857ae501439f62ab6d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-06 20:54:12 -0300

    Aula 03 06

--- Android Kotlin Developer/Aula-0227/MoneyMask/app/src/main/java/br/com/fiap/moneymask/MainActivity.kt
+++ Android Kotlin Developer/Aula-0227/MoneyMask/app/src/main/java/br/com/fiap/moneymask/MainActivity.kt
@@ -11,20 +11,34 @@ class MainActivity : AppCompatActivity() {
 //    val toggleButton : Button = findViewById(R.id.toggle_button)
     lateinit var binding: ActivityMainBinding
     private val mask: String = "******"
-    private val value: String? = null
+    private var value: String? = null
+    private var buttonState : ButtonState = ButtonState.HIDE_VALUE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //        toggle_button.setText("Text")
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.moneyCurrency
+        //binding.moneyCurrency
         setContentView(binding.root)
+        clickToggleButton()
     }
 
     fun clickToggleButton(){
-        value?.length
+        //value?.length
+        value = binding.moneyValue.text.toString();
         binding.toggleButton.setOnClickListener{
 
+            when(buttonState) {
+                ButtonState.HIDE_VALUE -> {
+                    binding.moneyValue.text = mask
+                    buttonState = ButtonState.SHOW_VALUE
+                }
+                ButtonState.SHOW_VALUE -> {
+                    binding.moneyValue.text = value
+                    buttonState = ButtonState.HIDE_VALUE
+                }
+            }
+            binding.toggleButton.text = getString(buttonState.buttonTextResId)
         }
     }
 }
