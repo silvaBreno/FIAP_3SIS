46ca9a64cd7334251e0f7ab79d80d4588009a529
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-20 20:54:51 -0300

    Aula 20 03

--- Android Kotlin Developer/Aula-0227/MoneyMask/app/src/main/java/br/com/fiap/moneymask/MainActivity.kt
+++ Android Kotlin Developer/Aula-0227/MoneyMask/app/src/main/java/br/com/fiap/moneymask/MainActivity.kt
@@ -23,6 +23,30 @@ class MainActivity : AppCompatActivity() {
         clickToggleButton()
     }
 
+    override fun onStart() {
+        super.onStart()
+        showValue()
+        binding.root.setBackgroundColor(resources.getColor(R.color.black))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showValue()
+        binding.root.setBackgroundColor(resources.getColor(R.color.purple_200))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        hideValue()
+        binding.root.setBackgroundColor(resources.getColor(R.color.white))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        hideValue()
+        binding.root.setBackgroundColor(resources.getColor(R.color.teal_700))
+    }
+
     fun clickToggleButton(){
         //value?.length
         value = binding.moneyValue.text.toString();
@@ -30,15 +54,27 @@ class MainActivity : AppCompatActivity() {
 
             when(buttonState) {
                 ButtonState.HIDE_VALUE -> {
-                    binding.moneyValue.text = mask
-                    buttonState = ButtonState.SHOW_VALUE
+                    hideValue()
                 }
                 ButtonState.SHOW_VALUE -> {
-                    binding.moneyValue.text = value
-                    buttonState = ButtonState.HIDE_VALUE
+                    showValue()
                 }
             }
-            binding.toggleButton.text = getString(buttonState.buttonTextResId)
+
         }
+    }
+
+
+    fun showValue(){
+        binding.moneyValue.text = value
+        buttonState = ButtonState.HIDE_VALUE
+        binding.toggleButton.text = getString(buttonState.buttonTextResId)
+    }
+
+    fun hideValue(){
+
+        binding.moneyValue.text = mask
+        buttonState = ButtonState.SHOW_VALUE
+        binding.toggleButton.text = getString(buttonState.buttonTextResId)
     }
 }
