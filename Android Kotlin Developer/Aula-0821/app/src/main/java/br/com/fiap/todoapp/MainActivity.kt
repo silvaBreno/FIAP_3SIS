2167c6053f8aef9e94ed032718f828092b8d294d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-21 20:58:02 -0300

    Aula-2108 TODOAPP Lista de Tarefas e Filtro

--- Android Kotlin Developer/Aula-0821/app/src/main/java/br/com/fiap/todoapp/MainActivity.kt
+++ Android Kotlin Developer/Aula-0821/app/src/main/java/br/com/fiap/todoapp/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.fiap.todoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fiap.todoapp.databinding.ActivityMainBinding
+import br.com.fiap.todoapp.databinding.ViewFilterItemBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupFilters()
+    }
+
+    private fun setupFilters(){
+        //a funcao values retorna todos os valores q eu passei la no ENUM
+        TaskStatus.values().forEach {
+            val filterOption = ViewFilterItemBinding.inflate(
+                layoutInflater,
+                binding.taskFilters,
+                false
+            )
+            //.filter pq eu dei o nome do id como "filter"
+            filterOption.filter.text = it.title
+            //a funcao addView espera que eu passe a view do Chip group como parametro para retornar a propria view
+            binding.taskFilters.addView(filterOption.root)
+        }
     }
 }
