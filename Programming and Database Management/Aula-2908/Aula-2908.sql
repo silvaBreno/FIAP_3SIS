296c2dfab74d1a163938c9c6197d10bdd9b8e90e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-29 20:20:47 -0300

    Aula 2908

---
+++ Programming and Database Management/Aula-2908/Aula-2908.sql
@@ -0,0 +1,16 @@
+/* Criar um bloco PL/SQL que valide um número de CPF informado pelo usuário.
+
+O bloco deve retornar, após informado o CPF se ele é válido ou inválido.
+
+Crie pelo menos uma exceção no BLOCO. Garanta que somente CPFs com 11 dígitos sejam validados */
+
+SET SERVEROUT ON
+
+//REGRAS:
+
+DECLARE
+
+BEGIN
+
+END;
+/
